700463c2075e25a310ce8e521e544b3fd6eb740d
msdev-mauricio <146637000+msdev-mauricio@users.noreply.github.com>
2023-10-01 08:44:06 -0300

    Create configplugin.js

---
+++ configplugin.js
@@ -0,0 +1,8 @@
+
+    window.consultornet=window.consultornet||{};
+    window.consultornet.compreEPague={
+        ativo: true,
+        tipo: "todos os produtos",
+        quantidadeTotal: 3,
+        quantidadePaga: 2
+    }
